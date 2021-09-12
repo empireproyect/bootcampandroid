c91eb17bcdd90bc4ad5c2a756c9b13b710966c11
Félix <lujan.felix80@gmail.com>
2021-09-12 11:06:45 +0200

    configure test

---
+++ app/src/androidTest/java/app/company/sportpop/HiltTestRunner.kt
@@ -0,0 +1,17 @@
+package app.company.sportpop
+
+import android.app.Application
+import android.content.Context
+import com.karumi.shot.ShotTestRunner
+import dagger.hilt.android.testing.HiltTestApplication
+
+class HiltTestRunner: ShotTestRunner() {
+
+    override fun newApplication(
+        cl: ClassLoader?,
+        className: String?,
+        context: Context?
+    ): Application {
+        return super.newApplication(cl, HiltTestApplication::class.java.name, context)
+    }
+}
